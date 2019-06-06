55ada116cd2b918627117c938b1f7c6465a3175d
Sven Püschel <spueschel89@gmail.com>
2019-06-06 15:28:33 +0200

    Fixed appcompat_thor.py now working

--- Ingest/appcompat_thor.py
+++ Ingest/appcompat_thor.py
@@ -4,6 +4,9 @@ from ingest import Ingest
 import re
 from appAux import loadFile
 from datetime import datetime
+import ntpath
+import sys 
+import os 
 
 logger = logging.getLogger(__name__)
 # Module to ingest entries from the appcombat module of the APT-Scanner THOR and SPARK.
@@ -11,7 +14,8 @@ logger = logging.getLogger(__name__)
 
 class Appcompat_THOR(Ingest):
     ingest_type = "appcompat_thor"
-    file_name_filter = '(?:\S*).(txt|log)$'
+    # file_name_filter = '(?:\S*).(txt|log)$'
+    file_name_filter = "(?:.*)(?:\/|\\\)(.*)\.txt$"
 
     def __init__(self):
         super(Appcompat_THOR, self).__init__()
@@ -32,7 +36,7 @@ class Appcompat_THOR(Ingest):
             m = r_hostname.match(r)
             if m:
                 hostname = m.group("HOSTNAME").upper() 
-                logger.debug("HOSTNAME found in THOR/SPARK report: %s" % hostname)
+                logger.info("HOSTNAME found in THOR/SPARK report: %s" % hostname)
         
         if hostname is None:
             logger.error("Could not extract hostname from THOR/SPARK report file.")
@@ -42,32 +46,54 @@ class Appcompat_THOR(Ingest):
 
     def processFile(self, file_fullpath, hostID, instanceID, rowsData):
         rowNumber = 0
+        minSQLiteDTS = datetime(1, 1, 1, 0 ,0 ,0)
+        maxSQLiteDTS = datetime(9999, 12, 31, 0, 0, 0)
+
         # Open the THOR/SPARK report file
-        file_object = loadFile(file_name_fullpath)
+        file_object = loadFile(file_fullpath)
         rows = file_object.read().splitlines()[1:]
         file_object.close()
+        # r_amcacheTHOR = re.compile(r".*MODULE: (Amcache) MESSAGE: (\S+) (.*)entry (FILE:|(.*) FILE:) (?P<FILE>.*) SHA1: (?P<SHA1>\w+) SIZE: (?P<SIZE>(None|\d)) DESC: (?P<DESC>.*) FIRST_RUN: (?P<FIRSTRUN>.*) CREATED: (?P<CREATED>.*) PRODUCT: (?P<PRODUCT>.*) COMPANY: (?P<COMPANY>.*)")
+        r_appcompatTHOR = re.compile(r".*MODULE: SHIMCache MESSAGE: (\S+) (.*)entry (FILE:|(.*) FILE:) (?P<FILE>.*) DATE: (?P<DATE>.*) TYPE: (?P<TYPE>.*) HIVEFILE: (?P<HIVE>.*) EXTRAS: (?P<EXTRAS>.*) (?P<EXEC>True|False) MD5: (?P<MD5>.*) (?P<SHA1>.*) (?P<SHA256>.*)")
         del file_object
 
-        r_amcacheTHOR = re.compile(r".*MODULE: (Amcache) MESSAGE: (\S+) (.*)entry (FILE:|(.*) FILE:) (?P<FILE>.*) SHA1: (?P<SHA1>\w+) SIZE: (?P<SIZE>(None|\d)) DESC: (?P<DESC>.*) FIRST_RUN: (?P<FIRST_RUN>.*) CREATED: (?P<CREATED>.*) PRODUCT: (?P<PRODUCT>.*) COMPANY: (?P<COMPANY>.*)")
-        
         assert (rows is not None)
         for r in rows:
-            m = r_amcacheTHOR.match(r)
+            m = r_appcompatTHOR.match(r)
             if m:
-                
-                namedrow = settings.EntriesFields(HostID=hostID, EntryType=settings.__APPCOMPAT__, RowNumber=rowNumber,
-                FirstRun = unicode(m.group('FIRST_RUN')), 
-                Created= unicode(m.group('CREATED')),
-                FilePath = unicode(m.group('FILE').rsplit('\\', 1)[0]),
-                FileName = unicode(m.group('FILE').rsplit('\\', 1)[1]),
+                logger.info("MATCH %s" % r) 
+                try:
+
+                    # Convert to timestmaps:
+                    if m.group('DATE') != 'N/A':
+                        tmp_date = datetime.strptime(m.group('DATE'), "%Y-%m-%d %H:%M:%S")
+                        logger.info("DATE: %s" % tmp_date)
+                    else:
+                        logger.info("DATE broken: %s" % m.group('DATE'))
+                        tmp_date = minSQLiteDTS
+
+                except Exception as e:
+                    print("crap")
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                    logger.info("Exception processing row (%s): %s [%s / %s / %s]" % (
+                    e.message, unicode(ntpath.split(m.group('FILE'))[0]), exc_type, fname, exc_tb.tb_lineno))
+                    # e.message, unicode(ntpath.split(m.group('FILE'))[0]), exc_type, fname, exc_tb.tb_lineno))
+
+
+                path, filename = ntpath.split(m.group('FILE'))
+                namedrow = settings.EntriesFields(HostID=hostID, 
+                EntryType=settings.__APPCOMPAT__, 
+                RowNumber=rowNumber,
+                LastModified =tmp_date,
+                LastUpdate = tmp_date,
+                FilePath = unicode(path),
+                FileName = unicode(filename),
+                ExecFlag=str(m.group('EXEC')),
                 SHA1 = unicode(m.group('SHA1')),
-                Size = unicode(m.group('SIZE')),
-                Product = unicode(m.group('PRODUCT')),
-                Company = unicode(m.group('COMPANY')),
-                FileDescription = unicode(m.group('DESC')),
                 InstanceID=instanceID)
-                
+
                 rowsData.append(namedrow)
                 rowNumber += 1
-            else:
-                logger.warning("Entry regex failed for: %s - %s" (hostID, r))
+
+        logger.info("PARSED ROWS: %s" % len(rowsData))
